c0eb4ff416ba81bead5e883eb0f737d3742ed203
arthur <wr80340@smail.nchu.edu.tw>
2025-06-11 15:43:59 +0800

    #tmp save

--- test.py
+++ test.py
@@ -3,38 +3,76 @@ import json
 import uuid
 from collections import defaultdict
 from pprint import pprint
+from typing import Any, Dict, List, Union
 
 from pydantic import BaseModel
 
 from utils.pycparser_tools import extract_ast_from_filepath, from_dict, generate_c_code
 
+
+class Node(BaseModel):
+    # ---------------------------
+    uuid: uuid.UUID
+    # ---------------------------
+    rel_path: str
+    fname: str
+    # ---------------------------
+    method: str
+    name: Union[str, None] = None
+    # ---------------------------
+    code: str
+    ast: Dict
+    # ---------------------------
+    line: int
+    # ---------------------------
+
+
+def search_node(node: Dict[str, Union[str, List, Dict]], node_type: str):
+    for node_key, node_value in node.items():
+        if node_key == "_nodetype" and node_value == node_type:
+            print(node)
+
+        elif isinstance(node_value, Dict):
+            search_node(node=node_value, node_type=node_type)
+        elif isinstance(node_value, List):
+            for subnode in node_value:
+                if isinstance(subnode, Dict):
+                    search_node(node=subnode, node_type=node_type)
+                else:
+                    pass
+
+
+def find_func_name(node: Dict[str, Union[str, List, Dict]]) -> Union[str, None]:
+    match node["_nodetype"]:
+        case "Decl":
+            name = node.get("name")
+        case "FuncDecl":
+            name = node.get("type").get("declname")
+        case "FuncDef":
+            name = node.get("decl").get("name")
+        case "Typedef":
+            name = node.get("name")
+        case _:
+            name = None
+    return name
+
+
+# %%
 repo_ast = extract_ast_from_filepath(
     aim_filepath=r"examples/c_files",
     fake_libc_include_path=r"-Iutils/fake_libc_include",
 )
 
-
-# %%
-class Node(BaseModel):
-    uuid: uuid.UUID
-    rel_path: str
-    fname: str
-    code: str
-    ast: dict
-    line: int
-    method: str
-
-
-# %%
-# result
 graph_nodes = {}
 for filepath, file_content in repo_ast.items():
     print(filepath)
+    print("len of ", len(file_content))
     for block in file_content:
         id = uuid.uuid4()
         graph_nodes[str(id)] = Node(
             uuid=id,
             rel_path=block["coord"].split(":")[0],
+            name=find_func_name(block),
             fname=block["coord"].split(":")[0].split("/")[-1],
             code=generate_c_code(from_dict(block)),
             ast=block,
@@ -42,78 +80,11 @@ for filepath, file_content in repo_ast.items():
             method=block["_nodetype"],
         )
 
-        # print(generate_c_code(from_dict(block)))
-    # break
+graph_nodes[list(graph_nodes.keys())[0]]
 
-#     result = defaultdict(list)
 
-# for element in data["ext"]:
-#     result[element["_nodetype"]].append(element)
-# a2.append()
-# %%
+set([v.method for v in graph_nodes.values()])
 
-pprint(graph_nodes[list(graph_nodes.keys())[0]])
-# %%
-result.keys()
-# %%
-pprint(result["FuncDef"][0].keys())
-# %%
-for i in range(len(result["FuncDef"])):
-    pprint(result["FuncDef"][i]["decl"]["name"])
-    for block_item in result["FuncDef"][i]["body"]["block_items"]:
-        if block_item["_nodetype"] == "FuncCall":
-            pprint(block_item["name"]["name"])
-    print("*" * 10)
 
-# %%
-for func in from_dict(data).ext:
-    pprint(func.decl)
-    print(generate_c_code(func))
-    print("*" * 50)
-# %%
-Node(
-    uuid=UUID("dc5e4765-756e-40ef-852d-0edf02771f38"),
-    rel_path="examples/c_files/basic.c",
-    fname="basic.c",
-    code="int foo()\n{\n}\n\n",
-    ast={
-        "_nodetype": "FuncDef",
-        "coord": "examples/c_files/basic.c:1:5",
-        "decl": {
-            "_nodetype": "Decl",
-            "name": "foo",
-            "quals": [],
-            "align": [],
-            "storage": [],
-            "funcspec": [],
-            "coord": "examples/c_files/basic.c:1:5",
-            "type": {
-                "_nodetype": "FuncDecl",
-                "coord": "examples/c_files/basic.c:1:5",
-                "type": {
-                    "_nodetype": "TypeDecl",
-                    "declname": "foo",
-                    "quals": [],
-                    "align": None,
-                    "coord": "examples/c_files/basic.c:1:5",
-                    "type": {
-                        "_nodetype": "IdentifierType",
-                        "names": ["int"],
-                        "coord": "examples/c_files/basic.c:1:1",
-                    },
-                },
-                "args": None,
-            },
-            "bitsize": None,
-            "init": None,
-        },
-        "body": {
-            "_nodetype": "Compound",
-            "coord": "examples/c_files/basic.c:1:1",
-            "block_items": None,
-        },
-        "param_decls": None,
-    },
-    line=1,
-    method="FuncDef",
-)
+for v in graph_nodes.values():
+    search_node(node=v.ast, node_type="Typedef")
